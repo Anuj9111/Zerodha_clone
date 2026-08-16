d51731775c9ac2bb68454399ba7dde3046cd200e
Anuj9111 <anuj9111yadav@gmail.com>
2026-08-16 22:53:32 +0530

    updated backend

--- backend/util/SecretToken.js
+++ backend/util/SecretToken.js
@@ -6,7 +6,7 @@ dotenv.config();
 const createSecretToken =(id)=>{
     return jwt.sign(
         {id},
-        process.env.TOKEN_KEY,
+        process.env.JWT_SECRET,
         {
            expiresIn:3*24*60*60, 
         }
